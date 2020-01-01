96ee6c8821b570a18fb248ee5dde56ab5ef0eb24
João Damazio <joaohd98@gmail.com>
2020-01-01 18:34:59 -0300

    Add architecture of service

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+export class Service {
+
+  static makeGetRequest = (url: string, parameters: object = {}, header: object = {}) => {
+
+  };
+
+  static makePostRequest = (url: string, parameters: object = {}, header: object = {}) => {
+
+  };
+
+  private static makeRequest = () => {
+
+
+  };
+
+}
